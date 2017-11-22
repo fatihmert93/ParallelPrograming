10c81825095135bd32ee65cede7f2212fbfb73c5
Fatih Mert <31659497+fatihmert-medyanet@users.noreply.github.com>
2017-11-22 10:49:31 +0300

    barrier, child task and continuations tasks inserted

---
+++ BarrierApp/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BarrierApp
+{
+    class Program
+    {
+        private static Barrier barrier = new Barrier(2, b =>
+        {
+            Console.WriteLine($"Phase {b.CurrentPhaseNumber} is finished");
+            
+        });
+
+        public static void Water()
+        {
+            Console.WriteLine("Putting the kettle on (takes a bit longer)");
+            Thread.Sleep(2000);
+            barrier.SignalAndWait(); // 2
+            Console.WriteLine("Pouring water into cup."); // 0
+            barrier.SignalAndWait(); // 1
+            Console.WriteLine("Putting the kettle away.");
+        }
+
+        public static void Cup()
+        {
+            Console.WriteLine("Finding the nicest cup of tea (fast)");
+            barrier.SignalAndWait(); // 1
+            Console.WriteLine("Adding tea.");
+            barrier.SignalAndWait();
+            Console.WriteLine("Adding sugar.");
+        }
+
+        static void Main(string[] args)
+        {
+            var water = Task.Factory.StartNew(Water);
+            var cup = Task.Factory.StartNew(Cup);
+
+            var tea = Task.Factory.ContinueWhenAll(new[] {water, cup}, tasks =>
+            {
+                Console.WriteLine("Enjoy your cup of tea.");
+            });
+            tea.Wait();
+        }
+    }
+}
---
+++ ChildTasks/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ChildTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var parent = new Task(() =>
+            {
+                var child = new Task(() =>
+                {
+                    Console.WriteLine("Child task starting");
+                    Thread.Sleep(3000);
+                    Console.WriteLine("Child task finishing");
+                }, TaskCreationOptions.AttachedToParent);
+
+                var completionHandler = child.ContinueWith(t =>
+                {
+                    Console.WriteLine($"Hooray, task {t.Id}'s state is {t.Status}");
+                }, TaskContinuationOptions.AttachedToParent | TaskContinuationOptions.OnlyOnRanToCompletion);
+
+                var failHandler = child.ContinueWith(t =>
+                {
+                    Console.WriteLine($"Ooops, task {t.Id}'s state is {t.Status}");
+                }, TaskContinuationOptions.AttachedToParent | TaskContinuationOptions.OnlyOnFaulted);
+
+
+
+                child.Start();
+            });
+            parent.Start();
+
+            try
+            {
+                parent.Wait();
+            }
+            catch (AggregateException ae)
+            {
+                ae.Handle(e => true);
+            }
+        }
+    }
+}
